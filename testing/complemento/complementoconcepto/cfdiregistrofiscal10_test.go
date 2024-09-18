9e4d03bddb7c25bb87d66e6cc6da22e87374a895
Hector Madrid <hector.madrid@kuantik.mx>
2024-09-18 13:04:42 -0600

    [TEST] cfdiregistrofiscal10 test correcto [0.0.12]
    Serializado de XML full de certificado de destruccion 1.0 y test de corriendo correctamente

---
+++ testing/complemento/complementoconcepto/cfdiregistrofiscal10_test.go
@@ -0,0 +1,27 @@
+package complementoconcepto
+
+import (
+	"encoding/xml"
+	registrofiscal "github.com/SaulEnriqueMR/kore-models/models/complemento/complementoconcepto"
+	testing2 "github.com/SaulEnriqueMR/kore-models/testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func GetCfdiRegistroFiscalForTest(filename string, t *testing.T) (registrofiscal.CfdiRegistroFiscal10, error) {
+	data := testing2.GetFileContentForTest(filename, t)
+	var parsed registrofiscal.CfdiRegistroFiscal10
+	errUnmarshal := xml.Unmarshal(data, &parsed)
+	assert.NoError(t, errUnmarshal)
+	return parsed, errUnmarshal
+}
+
+func TestCfdiRegistroFiscal10(t *testing.T) {
+	cfdiRegistroFiscal, _ := GetCfdiRegistroFiscalForTest("./cfdiregistrofiscal10.xml", t)
+	InternalTestFullAtributesCfdiRegistroFiscal10(t, cfdiRegistroFiscal)
+}
+
+func InternalTestFullAtributesCfdiRegistroFiscal10(t *testing.T, registroFiscal registrofiscal.CfdiRegistroFiscal10) {
+	assert.Equal(t, "1.0", registroFiscal.Version)
+	assert.Equal(t, "8123831283128383", registroFiscal.Folio)
+}

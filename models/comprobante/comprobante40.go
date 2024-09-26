f415697b38ee9aec158718de4af729548e953570
Héctor Bustos Sánchez <hector.bustos@kuantik.mx>
2024-09-26 16:11:34 -0600

    [FEAT] Add InformacionAdicional and Cancelacion condition [0.0.86]

--- models/comprobante/comprobante40.go
+++ models/comprobante/comprobante40.go
@@ -176,8 +176,20 @@ func (c *Comprobante40) UnmarshalXML(d *xml.Decoder, start xml.StartElement) err
 	c.FechaEmision = fechaEmision
 	c.Comprobante = true
 	c.Vigente = nil
-	c.InformacionAdicional.StampedByKuantik = nil
-	c.Cancelacion.CanceledByKuantik = nil
+	if c.InformacionAdicional != nil {
+		c.InformacionAdicional.StampedByKuantik = nil
+	} else {
+		c.InformacionAdicional = &documentofiscaldigital.InformacionAdicional{
+			StampedByKuantik: nil,
+		}
+	}
+	if c.Cancelacion != nil {
+		c.Cancelacion.CanceledByKuantik = nil
+	} else {
+		c.Cancelacion = &documentofiscaldigital.Cancelacion{
+			CanceledByKuantik: nil,
+		}
+	}
 
 	if c.Complemento.TimbreFiscalDigital != nil {
 		tfd := c.Complemento.TimbreFiscalDigital.TimbreFiscalDigital11

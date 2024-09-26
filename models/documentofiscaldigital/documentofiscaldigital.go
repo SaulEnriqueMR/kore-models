c15715074e2ae0029fbaec080ec6253e07ba60a8
Héctor Bustos Sánchez <hector.bustos@kuantik.mx>
2024-09-26 13:53:18 -0600

    FIX] Add StampedByKuantik, Vigencia and CanceledByKuantik optional [0.0.813]

--- models/documentofiscaldigital/documentofiscaldigital.go
+++ models/documentofiscaldigital/documentofiscaldigital.go
@@ -5,10 +5,10 @@ import (
 )
 
 type DocumentoFiscalDigital struct {
-	FechaEmision  time.Time `bson:"FechaEmision"`
-	Uuid          string    `bson:"Uuid"`
-	FechaTimbrado time.Time `bson:"FechaTimbrado"`
-	// Vigente              bool                 `bson:"Vigente"` // Add this field to Kore
+	FechaEmision         time.Time             `bson:"FechaEmision"`
+	Uuid                 string                `bson:"Uuid"`
+	FechaTimbrado        time.Time             `bson:"FechaTimbrado"`
+	Vigente              *bool                 `bson:"Vigente,omitempty"`
 	CadenaOriginal       string                `bson:"CadenaOriginal"`
 	InformacionAdicional *InformacionAdicional `xml:"InformacionAdicional" bson:"InformacionAdicional"`
 	Cancelacion          *Cancelacion          `xml:"Cancelacion" bson:"Cancelacion"`
@@ -16,16 +16,16 @@ type DocumentoFiscalDigital struct {
 }
 
 type InformacionAdicional struct {
-	// StampedByKuantik bool    `bson:"StampedByKuantik"` // Add this field to Kore
-	Comentario *string `bson:"Comentario,omitempty"`
-	Desgloce   *string `bson:"Desgloce,omitempty"`
-	NotaPie    *string `bson:"NotaPie,omitempty"`
-	IdProyecto *string `bson:"IdProyecto,omitempty"`
-	Categoria  *string `bson:"Categoria,omitempty"`
+	StampedByKuantik *bool   `bson:"StampedByKuantik,omitempty"` // Add this field to Kore
+	Comentario       *string `bson:"Comentario,omitempty"`
+	Desgloce         *string `bson:"Desgloce,omitempty"`
+	NotaPie          *string `bson:"NotaPie,omitempty"`
+	IdProyecto       *string `bson:"IdProyecto,omitempty"`
+	Categoria        *string `bson:"Categoria,omitempty"`
 }
 
 type Cancelacion struct {
-	// CanceledByKuantik  bool       `bson:"CanceledByKuantik"` // Add this field to Kore
+	CanceledByKuantik  *bool      `bson:"CanceledByKuantik,omitempty"` // Add this field to Kore
 	MotivoCancelacion  *string    `bson:"MotivoCancelacion,omitempty"`
 	FolioSustitucion   *string    `bson:"FolioSustitucion,omitempty"`
 	EstatusCancelacion *string    `bson:"EstatusCancelacion,omitempty"`

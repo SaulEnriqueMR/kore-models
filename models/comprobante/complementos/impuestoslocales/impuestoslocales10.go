45d6e41c28d0854163bdf809d803aec0b57afc9c
Saúl Enrique Melchor Ramírez <saul.melchor@kuantik.mx>
2024-09-27 17:05:06 -0600

    [FIX] Rename impuestos locales [0.0.108]

--- models/comprobante/complementos/impuestoslocales/impuestoslocales10.go
+++ models/comprobante/complementos/impuestoslocales/impuestoslocales10.go
@@ -1,21 +1,21 @@
 package impuestoslocales
 
 type ImpuestoLocales10 struct {
-	Version            string                        `xml:"version,attr" bson:"Version"`
-	TotalRetenciones   float64                       `xml:"TotaldeRetenciones,attr" bson:"TotalRetenciones"`
-	TotalTraslados     float64                       `xml:"TotaldeTraslados,attr" bson:"TotalTraslados"`
-	RetencionesLocales *[]RetencionesLocalesImpLoc10 `xml:"RetencionesLocales" bson:"RetencionesLocales,omitempty"`
-	TrasladosLocales   *[]TrasladosLocalesImpLoc10   `xml:"TrasladosLocales" bson:"TrasladosLocales,omitempty"`
+	Version          string                        `xml:"version,attr" bson:"Version"`
+	TotalRetenciones float64                       `xml:"TotaldeRetenciones,attr" bson:"TotalRetenciones"`
+	TotalTraslados   float64                       `xml:"TotaldeTraslados,attr" bson:"TotalTraslados"`
+	Retenciones      *[]RetencionesLocalesImpLoc10 `xml:"RetencionesLocales" bson:"Retenciones,omitempty"`
+	Traslados        *[]TrasladosLocalesImpLoc10   `xml:"TrasladosLocales" bson:"Traslados,omitempty"`
 }
 
 type RetencionesLocalesImpLoc10 struct {
-	ImpLocRetenido string  `xml:"ImpLocRetenido,attr" bson:"ImpLocRetenido"`
-	TasaRetencion  float64 `xml:"TasadeRetencion,attr" bson:"TasaRetencion"`
-	Importe        float64 `xml:"Importe,attr" bson:"Importe"`
+	Impuesto string  `xml:"ImpLocRetenido,attr" bson:"Impuesto"`
+	Tasa     float64 `xml:"TasadeRetencion,attr" bson:"Tasa"`
+	Importe  float64 `xml:"Importe,attr" bson:"Importe"`
 }
 
 type TrasladosLocalesImpLoc10 struct {
-	ImpLocTrasladado string  `xml:"ImpLocTrasladado,attr" bson:"ImpLocTrasladado"`
-	TasaTraslado     float64 `xml:"TasadeTraslado,attr" bson:"TasaTraslado"`
-	Importe          float64 `xml:"Importe,attr" bson:"Importe"`
+	Impuesto string  `xml:"ImpLocTrasladado,attr" bson:"Impuesto"`
+	Tasa     float64 `xml:"TasadeTraslado,attr" bson:"Tasa"`
+	Importe  float64 `xml:"Importe,attr" bson:"Importe"`
 }
--- testing/comprobante/complementos/implocales/implocales_test.go
+++ testing/comprobante/complementos/implocales/implocales_test.go
@@ -21,8 +21,8 @@ func GetImpuestosLocales10ForTest(filename string, t *testing.T) (implocales10.I
 func TestFullImpLocales10(t *testing.T) {
 	implocales, _ := GetImpuestosLocales10ForTest("./implocales.xml", t)
 	InternalTestFullAttributes(t, implocales)
-	InternalTestFullRetencionesLocales(t, implocales.RetencionesLocales)
-	InternalTestFullTrasladosLocales(t, implocales.TrasladosLocales)
+	InternalTestFullRetencionesLocales(t, implocales.Retenciones)
+	InternalTestFullTrasladosLocales(t, implocales.Traslados)
 }
 
 func InternalTestFullAttributes(t *testing.T, implocales implocales10.ImpuestoLocales10) {
@@ -35,16 +35,16 @@ func InternalTestFullAttributes(t *testing.T, implocales implocales10.ImpuestoLo
 func InternalTestFullRetencionesLocales(t *testing.T, implocales *[]implocales10.RetencionesLocalesImpLoc10) {
 	assert.NotNil(t, implocales)
 	first := (*implocales)[0]
-	assert.Equal(t, "ISR", first.ImpLocRetenido)
+	assert.Equal(t, "ISR", first.Impuesto)
 	assert.Equal(t, 1500.00, first.Importe)
-	assert.Equal(t, 15.00, first.TasaRetencion)
+	assert.Equal(t, 15.00, first.Tasa)
 }
 
 func InternalTestFullTrasladosLocales(t *testing.T, implocales *[]implocales10.TrasladosLocalesImpLoc10) {
 	assert.NotNil(t, implocales)
 	first := (*implocales)[0]
-	assert.Equal(t, "IVA", first.ImpLocTrasladado)
+	assert.Equal(t, "IVA", first.Impuesto)
 	assert.Equal(t, 3000.00, first.Importe)
-	assert.Equal(t, 16.00, first.TasaTraslado)
+	assert.Equal(t, 16.00, first.Tasa)
 
 }

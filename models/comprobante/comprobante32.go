22c7a66a9edfa59bb80e7a0ddcb7043a6a13cfe2
Héctor Bustos Sánchez <hector.bustos@kuantik.mx>
2024-10-10 17:36:56 -0600

    [FIX] Add CadenaOriginal Cast [1.0.3]

--- models/comprobante/comprobante32.go
+++ models/comprobante/comprobante32.go
@@ -159,7 +159,7 @@ func (c *Comprobante32) UnmarshalXML(d *xml.Decoder, start xml.StartElement) err
 			c.Uuid = strings.ToUpper(tfd.Uuid)
 		}
 	}
-	c.CadenaOriginal = helpers.CreateCadenaOriginal(c)
+	c.CadenaOriginal = helpers.CreateCadenaOriginal(*c)
 
 	return nil
 }

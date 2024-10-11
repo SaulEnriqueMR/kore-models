0e6cd045a096fa0112964d2224a5058c6a362a9b
Saúl Enrique Melchor Ramírez <saul.melchor@kuantik.mx>
2024-10-10 19:05:30 -0600

    [FIX] Cadena original en atributos vacíos [1.0.4]

--- models/helpers/cadenaoriginal.go
+++ models/helpers/cadenaoriginal.go
@@ -6,69 +6,102 @@ import (
 	"strings"
 )
 
-func CreateCadenaOriginal(s interface{}) string {
-	v := reflect.ValueOf(s)
-	if v.Kind() != reflect.Struct {
-		return ""
-	}
-
-	ignoreMap := map[string]struct{}{
-		"Sello":                  {},
-		"Certificado":            {},
-		"NoCertificado":          {},
-		"XMLName":                {},
-		"SelloCfd":               {},
-		"NoCertificadoSat":       {},
-		"SelloSat":               {},
-		"DocumentoFiscalDigital": {},
-	}
-
+func CreateCadenaOriginal(v interface{}) string {
 	var sb strings.Builder
-	sb.WriteString("||")
+	val := reflect.ValueOf(v)
 
-	for i := 0; i < v.NumField(); i++ {
-		field := v.Field(i)
-		fieldType := v.Type().Field(i)
+	processValue(val, &sb)
 
-		// Check if the field name is in the ignore map
-		if _, found := ignoreMap[fieldType.Name]; found {
-			continue // Skip this field
-		}
-
-		var value string
-
-		if field.CanInterface() { // Check if the field can be accessed
-			switch field.Kind() {
-			case reflect.Bool:
-				continue
-			case reflect.Ptr:
-				// Handle pointer types
-				if field.IsNil() {
-					value = ""
-				} else {
-					value = fmt.Sprintf("%v", CreateCadenaOriginal(field.Elem().Interface()))
-				}
-			case reflect.Struct:
-				// Handle nested structs
-				value = CreateCadenaOriginal(field.Interface())
-			case reflect.Slice, reflect.Array:
-				// Handle arrays and slices
-				var sliceValues []string
-				for j := 0; j < field.Len(); j++ {
-					sliceValues = append(sliceValues, fmt.Sprintf("%v", CreateCadenaOriginal(field.Index(j).Interface())))
-				}
-				value = strings.Join(sliceValues, ",")
-			default:
-				value = fmt.Sprintf("%v", field.Interface())
-			}
-		} else {
-			value = "" // If the field cannot be accessed, assign an empty string
-		}
-
-		sb.WriteString(value)
-		sb.WriteString("|")
+	// Remove the trailing "|" if present
+	str := sb.String()
+	if len(str) > 0 && str[len(str)-1] == '|' {
+		str = str[:len(str)-1]
 	}
 
+	aux := sb.String()
+	sb.Reset()
+	sb.WriteString("||")
+	sb.WriteString(aux)
 	sb.WriteString("|")
 	return sb.String()
+}
+
+var ignoreMap = map[string]struct{}{
+	"Sello":                  {},
+	"Certificado":            {},
+	"NoCertificado":          {},
+	"XMLName":                {},
+	"SelloCfd":               {},
+	"NoCertificadoSat":       {},
+	"SelloSat":               {},
+	"DocumentoFiscalDigital": {},
+}
+
+func processValue(val reflect.Value, sb *strings.Builder) {
+	if !val.IsValid() || isEmptyValue(val) {
+		return // Skip invalid or empty values
+	}
+
+	switch val.Kind() {
+	case reflect.Ptr:
+		if !val.IsNil() {
+			processValue(val.Elem(), sb) // Dereference and process non-nil pointers
+		}
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			field := val.Field(i)
+			fieldType := val.Type().Field(i)
+
+			if val.Type().Field(i).PkgPath == "" { // Only process exported fields
+				// Accessing field name
+				fieldName := fieldType.Name
+				_, shouldSkip := ignoreMap[fieldName]
+				if !shouldSkip {
+					processValue(field, sb)
+				}
+			}
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			processValue(val.Index(i), sb)
+		}
+	case reflect.String:
+		if val.String() != "" { // Only append non-empty strings
+			sb.WriteString(val.String() + "|")
+		}
+	case reflect.Int, reflect.Int64, reflect.Int32:
+		sb.WriteString(fmt.Sprintf("%d|", val.Int()))
+	case reflect.Float64, reflect.Float32:
+		sb.WriteString(fmt.Sprintf("%f|", val.Float()))
+	case reflect.Bool:
+		// sb.WriteString(fmt.Sprintf("%t|", val.Bool()))
+		return
+	default:
+		sb.WriteString(fmt.Sprintf("%v|", val.Interface()))
+	}
+}
+
+// Helper function to determine if a value is empty (e.g., empty string, zero int, nil pointer)
+func isEmptyValue(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.String:
+		return v.Len() == 0
+	case reflect.Array, reflect.Slice:
+		return v.Len() == 0
+	case reflect.Map, reflect.Chan:
+		return v.Len() == 0
+	case reflect.Ptr, reflect.Interface:
+		return v.IsNil()
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Struct:
+		return false // Structs are never considered "empty"
+	}
+	return false
 }
--- testing/helpers/cadenaoriginal_test.go
+++ testing/helpers/cadenaoriginal_test.go
@@ -20,5 +20,5 @@ func GetComprobanteForTest(filename string, t *testing.T) (comprobante2.Comproba
 func TestCreateCadenaOriginal(t *testing.T) {
 	comprobante, _ := GetComprobanteForTest("./comprobantecadenaoriginal.xml", t)
 	result := helpers.CreateCadenaOriginal(comprobante)
-	assert.Equal(t, "||4.0|||2024-04-29T00:00:55|||200||MXN||199.96|I|01||20000||||||EKU9003173C9|ESCUELA KEMPER URGATE|601||||||URE180429TM6|UNIVERSIDAD ROBOTICA ESPAÑOLA|86991|||601|G01|||SPR190613I52|||50211503||1|H87||Cigarros|200|200||02||||||||||||||||||||||||||||||||||||||||||||||1.1|3ea43e97-71bf-4ac5-a28e-9374ea9f8b45|2024-04-29T10:46:30||||||||SPR190613I52||||||||||||||||||", result)
+	assert.Equal(t, "||4.0|Serie|Folio|2024-04-29T00:00:55|99|CondicionesDePago|200.000000|MXN|199.960000|I|01|PPD|20000|EKU9003173C9|ESCUELA KEMPER URGATE|601|URE180429TM6|UNIVERSIDAD ROBOTICA ESPAÑOLA|86991|601|G01|SPR190613I52|50211503|1.000000|H87|Pieza|Cigarros|200.000000|200.000000|02|1.000000|002|Tasa|0.160000|0.160000|1.000000|001|Tasa|0.100000|0.100000|1.000000|002|Tasa|0.106666|0.100000|0.200000|0.160000|001|0.100000|002|0.100000|1.000000|002|Tasa|0.160000|0.160000|1.1|3ea43e97-71bf-4ac5-a28e-9374ea9f8b45|2024-04-29T10:46:30|SPR190613I52||", result)
 }

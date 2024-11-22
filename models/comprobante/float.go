3a7390f3ef19c4b68df3bbd4f902d035d8a7df25
Héctor Bustos Sánchez <hector.bustos@kuantik.mx>
2024-11-22 09:52:50 -0600

    [FIX] Add handler float error [1.0.28]

--- models/comprobante/float.go
+++ models/comprobante/float.go
@@ -2,6 +2,7 @@ package comprobante
 
 import (
 	"encoding/xml"
+	"fmt"
 	"strconv"
 )
 
@@ -10,7 +11,7 @@ type Float float64
 func (f *Float) UnmarshalXMLAttr(attr xml.Attr) error {
 	value, err := strconv.ParseFloat(attr.Value, 64)
 	if err != nil {
-		return err
+		return fmt.Errorf("error parsing float value: %w", err)
 	}
 	*f = Float(value)
 	return nil

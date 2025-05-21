6064e4696f64094d48cb9882c1baa1e488a94b78
Héctor Bustos Sánchez <hector.bustos@kuantik.mx>
2025-05-21 11:33:56 -0600

    [FEAT] Add Universal Search [1.0.83]

---
+++ models/comprobante/universal.go
@@ -0,0 +1,27 @@
+package comprobante
+
+type UniversalSearch struct {
+	Uuid           string `json:"Uuid" bson:"Uuid"`
+	CadenaOriginal string `json:"CadenaOriginal" bson:"CadenaOriginal"`
+}
+
+func (c *Comprobante32) ToUniversalSearch() *UniversalSearch {
+	return &UniversalSearch{
+		Uuid:           c.Uuid,
+		CadenaOriginal: c.CadenaOriginal,
+	}
+}
+
+func (c *Comprobante33) ToUniversalSearch() *UniversalSearch {
+	return &UniversalSearch{
+		Uuid:           c.Uuid,
+		CadenaOriginal: c.CadenaOriginal,
+	}
+}
+
+func (c *Comprobante40) ToUniversalSearch() *UniversalSearch {
+	return &UniversalSearch{
+		Uuid:           c.Uuid,
+		CadenaOriginal: c.CadenaOriginal,
+	}
+}
